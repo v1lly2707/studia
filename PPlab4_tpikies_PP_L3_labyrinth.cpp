e38254da1048f3ad72822e2bb9720f429523bcfa
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-15 22:00:38 +0200

    Create PPlab4_tpikies_PP_L3_labyrinth.cpp
    Write a program construcing a simple visualization for a game.
    The visualization depends on the mode of operation.
    
    INPUT:
    p pos1 ... posp
    m mine1 ... minem
    w wall1 ... wallw
    mode args
    
    
    The meaning is as follows:
     - p, m, w are the number of players, mines, walls respectively.
     - pos1 ... posp are positions of particular players -- the should be printed as corresponding numbers from 0 to p-1.
     - mine1 ... minem are positions of mines -- they should be printed as ^
     - wall1 ... wallw are positions of walls -- they should be printed as #
     - emptyspaces should be printed as .
    
    The number of players, mines, walls are bounded by 100, the board is contained in [0, 10]x[0, 10]
    The positions are given in 2D -- they are pairs y,x.
    Assume that an object (player / mine / etc) given by (y,x) occupies a square [y, y+1]x[x, x+1]
    Moreover y axis tends from up to bottom and x axis from left to right.
    
    For mode = 0, print the map in [0, 9] x [0, 9].
    For mode = 1, scan additional arguments y, x, dist.
                  Print the map in [y-dist, ..., y, ..., y+dist] x [x-dist, ..., x, ..., x+dist].
                  Keep in mind that it might be the case that y-dist < 0 or y + dist > SIZE, similarly for x dimension.
                  In this the program should print '_' in these fields.
    For mode = 2, scan additional arguments y, x, dist.
                  Print the map in [y-dist, ..., y, ..., y+dist] x [x-dist, ..., x, ..., x+dist], however take into account walls.
                  Precisely, assume that there is a wall at position (y, x') [or (y', x)].
                  In this case print the fields behind (from perspective of (y,x)) the wall as '?'.
                  In this (simplified) version it means that the fields have to be in the same row / column as wall.
    
    
    OUTPUT:
    visualization
    
    Example00:
    INPUT:
    3    0 0  1 2  3 4
    2    0 4  2 2
    2    1 1  4 4
    0
    
    OUTPUT:
    0...^.....
    .#1.......
    ..^.......
    ....2.....
    ....#.....
    ..........
    ..........
    ..........
    ..........
    ..........
    
    
    Example01:
    INPUT:
    3    0 0  1 2  3 4
    2    0 4  2 2
    2    1 1  4 4
    0
    
    OUTPUT:
    0...^.....
    .#1.......
    ..^.......
    ....2.....
    ....#.....
    ..........
    ..........
    ..........
    ..........
    ..........
    
    
    
    Example1:
    INPUT:
    5    0 0  1 2  3 4  3 7  9 9
    5    0 4  2 2  6 5  5 4  8 8
    3    1 1  4 4  7 6
    1 8 8 2
    
    OUTPUT:
    ...._
    #..._
    ..^._
    ...4_
    _____
    
    
    
    Example2:
    INPUT:
    7    0 0  1 2  3 4  4 3  2 6  8 1  3 8 
    6    0 4  2 2  2 3  2 4  3 2  5 7
    5    6 6  6 7  6 8  6 9  7 7
    2    8 7  2
    
    OUTPUT:
    .#?##
    ..#..
    .....
    .....
    _____

---
+++ PPlab4_tpikies_PP_L3_labyrinth.cpp
@@ -0,0 +1,77 @@
+//60% score
+#include <iostream>
+
+using namespace std;
+
+char GameMap[1005][1005] = {};
+
+void fillTheMap()
+{
+    for (int i = 0; i <= 10; i++)
+        for (int j = 0; j <= 10; j++)
+            GameMap[i][j] = '.';
+}
+int main()
+{
+
+    fillTheMap();
+    int p, x, y;
+    int a, b, dist;
+    cin >> p;
+    for (int i = 0; i < p; i++) // players coordinates
+    {
+        cin >> x >> y;
+        GameMap[x][y] = ((char)i) + 48;
+    }
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) // mines coordinates
+    {
+        cin >> x >> y;
+        GameMap[x][y] = '^';
+    }
+
+    int w;
+    cin >> w;
+    for (int i = 0; i < w; i++)
+    {
+        cin >> x >> y;
+        GameMap[x][y] = '#';
+    }
+    int mode;
+    cin >> mode;
+    if (mode == 0)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+                cout << GameMap[i][j];
+
+            cout << endl;
+        }
+    }
+    else if (mode == 1)
+    {
+        cin >> a >> b >> dist;
+        for (int i = a - dist; i <= a + dist; i++)
+        {
+
+            for (int j = b - dist; j <= b + dist; j++)
+            {
+
+                if (j < 0 || j>9 || i < 0 || i>9)
+                    cout << '_';
+                else
+                    cout << GameMap[i][j];
+            }
+
+
+
+
+            cout << endl;
+        }
+
+
+    }
+    return 0;
+}
