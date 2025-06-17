be980b3199019ce044eb844becabcfe0d913d5e6
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2025-06-17 18:20:35 +0200

    Delete SI/laby directory

--- SI/laby/lab2SI/data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def get_small():
-    knapsack = pd.read_csv('knapsack-small.csv')
-    return knapsack, 10
-
-def get_big():
-    knapsack = pd.read_csv('knapsack-big.csv')
-    return knapsack, 6404180
